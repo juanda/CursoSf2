93d4e0741435a783b58d7de0553c5f63ddca7b6b
Juan David Rodríguez <juandavid.rodriguez@ite.educacion.es>
2013-04-16 13:01:02 +0200

    algunos toquecitos

--- src/Jazzyweb/CursoSf2/LosServiciosBundle/Controller/DefaultController.php
+++ src/Jazzyweb/CursoSf2/LosServiciosBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
 
-    public function indexAction($name) {
-        return $this->render('JCSf2LosServiciosBundle:Default:index.html.twig', array('name' => $name));
+    public function indexAction() {
+        return $this->render('JCSf2LosServiciosBundle:Default:index.html.twig');
     }
 
     public function servicioBasicoAction() {
