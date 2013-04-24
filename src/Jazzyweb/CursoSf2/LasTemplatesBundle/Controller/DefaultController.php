6b1a831c2de5a8ec01e28cfd3f518d57ec658396
Juan David Rodríguez <juandavid.rodriguez@ite.educacion.es>
2013-04-24 11:26:16 +0200

    Plantillas twig

--- src/Jazzyweb/CursoSf2/LasTemplatesBundle/Controller/DefaultController.php
+++ src/Jazzyweb/CursoSf2/LasTemplatesBundle/Controller/DefaultController.php
@@ -4,14 +4,17 @@ namespace Jazzyweb\CursoSf2\LasTemplatesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('JCSf2TemplatesBundle:Default:index.html.twig');
+class DefaultController extends Controller {
+
+    public function inicioAction() {
+        return $this->render('JCSf2TemplatesBundle:Default:inicio.html.twig');
     }
     
-    public function inicioAction(){
-        return $this->render('JCSf2TemplatesBundle:Default:inicio.html.twig');
+    public function indexAction() {
+        return $this->render('JCSf2TemplatesBundle:Default:index.html.twig');
+    }
+
+    public function bootstrapAction() {
+        return $this->render('JCSf2TemplatesBundle:Default:bootstrap.html.twig');
     }
 }
