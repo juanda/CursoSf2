f44185a0bf0a17a40ccaf0caa0234a90d3b85a1f
juanda <juanda@momentum.(none)>
2013-05-05 17:10:22 +0200

    jugando con FOSUserBundle

--- src/Jazzyweb/CursoSf2/ElFOSUserBundle/JCSf2FOSUserBundle.php
+++ src/Jazzyweb/CursoSf2/ElFOSUserBundle/JCSf2FOSUserBundle.php
@@ -4,6 +4,9 @@ namespace Jazzyweb\CursoSf2\ElFOSUserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class JCSf2FOSUserBundle extends Bundle
-{
+class JCSf2FOSUserBundle extends Bundle {
+
+    public function getParent() {
+        return 'FOSUserBundle';
+    }
 }
