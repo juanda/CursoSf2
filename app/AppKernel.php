a48fa46777cb4e85f952484fba97fb80d8b757ec
Juan David Rodriguez <juandalibaba@gmail.com>
2013-10-07 11:20:14 +0200

    borro algunas cosas innecesarias

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel {
             new Jazzyweb\CursoSf2\LosControladoresYRutasBundle\JCSf2ControlRutasBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Jazzyweb\CursoSf2\ElFOSUserBundle\JCSf2FOSUserBundle(),
-            new Jazzyweb\ChismorreosBundle\JazzywebChismorreosBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Jazzyweb/ChismorreosBundle/JazzywebChismorreosBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Jazzyweb\ChismorreosBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class JazzywebChismorreosBundle extends Bundle
-{
-}
